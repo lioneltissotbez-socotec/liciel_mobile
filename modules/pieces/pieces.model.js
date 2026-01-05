a631a95f137642fb625b7ed4d7d77f2263239729
lioneltissotbez-socotec <lioneltissotbez@gmail.com>
2026-01-05 14:40:13 +0100

    Create pieces.model.js

---
+++ modules/pieces/pieces.model.js
@@ -0,0 +1,11 @@
+function createPiece() {
+  return {
+    id: crypto.randomUUID(),
+    batiment: "",
+    nom: "",
+    visite: true,
+    justification: "",
+    moyens: "",
+    photos: [] // { name, blob }
+  };
+}
