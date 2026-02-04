76e53d004024bd236f16cf658aab6875478c75aa
lioneltissotbez-socotec <lioneltissotbez@gmail.com>
2026-02-04 16:17:18 +0100

    Update description.ui.js

--- modules/description/description.ui.js
+++ modules/description/description.ui.js
@@ -62,7 +62,7 @@ return `<span style="
   border: 2px solid ${borderColor};
   background: ${bgColor};
   border-radius: 4px;
-  font-size: 11px;
+  font-size: 10px;
   white-space: nowrap;
   line-height: 1.3;
 ">
